0ea41110337c3abeab9435c03959e8042ca062b5
Fergal Mc Carthy <fmccarthy@suse.com>
2023-11-20 10:06:05 -0500

    Fix usage message for tools/get_account.py

--- tools/get_account.py
+++ tools/get_account.py
@@ -32,7 +32,7 @@ def main(argv):
     """Main entrypoint to the Account reset tool."""
 
     if len(argv) != 2:
-        print('Usage: python3 reset_account.py <account_id>')
+        print('Usage: python3 get_account.py <account_id>')
         return
 
     account_id = argv[1]
